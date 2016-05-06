f7a9627d7a5a05a08de0f7c99090fa39632cef60
Diwank Singh <diwank.singh@gmail.com>
2016-05-06 09:53:26 +0530

    Built files
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

---
+++ throttle.js
@@ -0,0 +1,54 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = throttle;
+
+var _streams = require("@pipes/core/streams");
+
+function throttle() {
+  var interval = arguments.length <= 0 || arguments[0] === undefined ? 0 : arguments[0];
+  var head = arguments.length <= 1 || arguments[1] === undefined ? true : arguments[1];
+
+
+  // Throttle state
+  var ready = head,
+      timer = void 0;
+
+  // Set timer
+  timer = global.setInterval(function () {
+    ready = true;
+  }, interval);
+
+  // Return stream
+  return new _streams.TransformStream({
+    transform: function transform(chunk, enqueue, done) {
+      // Check if throttle ready
+      if (ready) {
+
+        // Push and reset
+        enqueue(chunk);
+        ready = false;
+      }
+
+      return done();
+    },
+    flush: function flush(enqueue, close) {
+      // Clear timer
+      timer && global.clearInterval(timer);
+
+      close();
+    }
+  });
+}
+
+// Browserify compat
+// throttle :: Int -> TransformStream
+// throttle function takes an int n and
+// returns a transform stream that throttles
+// the incoming values by n ms, only producing
+// values every n ms and dropping the rest.
+//
+
+if (typeof module !== "undefined") module.exports = throttle;
