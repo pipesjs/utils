15553d5bc6dddcd84653d53c224e9a51313744e8
Diwank Singh <diwank.singh@gmail.com>
2017-05-20 11:51:39 -0400

    Added flow types to last
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- src/last.js
+++ src/last.js
@@ -1,16 +1,20 @@
+// @flow
+
 // last :: TransformStream
 // last function returns a transform stream
 // that takes the last value from the input stream
 // and enqueues it on the output stream.
 //
 
+import type { ReadableWritable } from "@pipes/core/streams";
+
 import Accumulate from "@pipes/core/accumulate";
 
-export default function last() {
-  return new Accumulate( (a, b) => b );
+export default function last(): ReadableWritable {
+  return new Accumulate( (a: mixed, b: mixed) => b );
 }
 
 // Browserify compat
 if ( typeof module !== "undefined" )
+  // $FlowFixMe
   module.exports = last;
-
