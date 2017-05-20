01f96725a691794d024f9bbbdd3ad5472f49e0e7
Diwank Singh <diwank.singh@gmail.com>
2017-05-20 11:55:38 -0400

    Added flow types to pick
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- src/pick.js
+++ src/pick.js
@@ -1,3 +1,5 @@
+// @flow
+
 // pick :: ...String -> TransformStream
 // pick function takes any number of strings
 // as arguments and returns a transform stream
@@ -5,11 +7,13 @@
 // incoming values.
 //
 
+import type { ReadableWritable } from "@pipes/core/streams";
+
 import Pipe from "@pipes/core/pipe";
 
-export default function pick(...props) {
+export default function pick(...props: Array<string>): ReadableWritable {
 
-  return new Pipe( chunk => {
+  return new Pipe( (chunk={}) => {
     let res = {};
 
     for ( let prop of props )
@@ -21,5 +25,5 @@ export default function pick(...props) {
 
 // Browserify compat
 if ( typeof module !== "undefined" )
+  // $FlowFixMe
   module.exports = pick;
-
