c88e60588aed594d41054206e6fe64faf37f2d42
Diwank Singh <diwank.singh@gmail.com>
2017-05-20 11:13:21 -0400

    Added flow types to compact
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- src/compact.js
+++ src/compact.js
@@ -1,18 +1,22 @@
+// @flow
+
 // compact :: TransformStream
 // compact function returns a transform stream
 // that spits out only truthy
 // values from the input stream.
 //
 
+import type { ReadableWritable } from "@pipes/core/streams";
+
 import pipe from "@pipes/core/pipe";
 
 export default function compact() {
-  return new pipe( function compacter(chunk) {
+  return new pipe( function compacter(chunk: mixed): ?mixed {
     return ( chunk ? chunk : void 0 );
   });
 }
 
 // Browserify compat
 if ( typeof module !== "undefined" )
+  // $FlowFixMe
   module.exports = compact;
-
