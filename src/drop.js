9f698da39c6c8c32b0b0440783524b8d1028888f
Diwank Singh <diwank.singh@gmail.com>
2017-05-20 11:34:06 -0400

    Added flow types to drop
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- src/drop.js
+++ src/drop.js
@@ -1,3 +1,5 @@
+// @flow
+
 // drop :: Int -> TransformStream
 // drop function takes an int n and
 // returns a transform stream
@@ -5,15 +7,17 @@
 // from the input stream.
 //
 
+import type { ReadableWritable } from "@pipes/core/streams";
+
 import pipe from "@pipes/core/pipe";
 
-export default function drop(count) {
-  return new pipe( function dropper(chunk) {
+export default function drop(count: number): ReadableWritable {
+  return new pipe( function dropper(chunk: mixed): ?mixed {
     return ( count-- > 0 ? void 0 : chunk );
   });
 }
 
 // Browserify compat
 if ( typeof module !== "undefined" )
+  // $FlowFixMe
   module.exports = drop;
-
